7b4043f89945d2c491f9ba75fd14c7e26e7bd652
jenmoncivais <jenmoncivais@users.noreply.github.com>
2015-02-14 15:02:52 -0600

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p = new Point(32, 24);
+		
+		System.out.println("Hello, world.");
+		p.print();
+	}
+
+}
